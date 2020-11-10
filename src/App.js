ba55b4c23ad42159b05ac7bbf805cc514e85b2aa
Plankiton <pl4nk1ton@gmail.com>
2020-11-10 12:31:44 -0300

    Falta a lista de extrato

--- src/App.js
+++ src/App.js
@@ -5,49 +5,56 @@ import './style.css';
 var api_url = 'http://localhost:5000/api';
 var down = 'https://img.icons8.com/metro/26/000000/down--v1.png';
 
-var h = {
-    headers: {
-        //'Control-Allow-Headers': 'Authorization',
-        //'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json'
+function getType(t) {
+    switch (t) {
+        case 'Trade':
+            return 'Transferência';
+        case 'TakeIn':
+            return 'Depósito';
+        case 'TakeOut':
+            return 'Saque';
+        default:
+            return 'Saldo';
     }
 }
 
 function App() {
     const [user, setUser] = useState(null);
+    const [history, setHistory] = useState(null);
+    const [loadHistory, setLoadHistory] = useState(false);
 
     return user == null ? (
         <div id="login">
             <form onSubmit={(e) => {
                 e.preventDefault();
 
-                var user = e.target[0].value;
+                var name = e.target[0].value;
                 var pass = e.target[1].value;
-                axios.get(`${api_url}/Client/${pass}/${user}`)
+                axios.get(`${api_url}/Client/byname/${pass}/${name}`)
                     .then((r) => {
-                        console.log(r);
-
                         setUser({
+                            id: r.data.id,
                             name: r.data.name,
                             password: pass,
-                            balance: r.data.balance
+                            balance: r.data.balance,
                         });
                     }).catch((e) => {
-                        console.log(`${api_url}/Client/${pass}/${user}`, e);
-
                         axios.post(`${api_url}/Client`, {
-                            name: user,
+                            name: name,
                             password: pass
                         }).then((r) => {
                             console.log(r);
 
                             setUser({
+                                id: r.data.id,
                                 name: r.data.name,
                                 password: pass,
-                                balance: r.data.balance
+                                balance: r.data.balance,
                             });
                         }).catch((e) => {
                             console.log(e);
+                        }).finally(() => {
+                            console.log(pass, name);
                         });
 
                     });
@@ -69,13 +76,47 @@ function App() {
             <h1 className="user">{user.name}</h1>
             <p className="balance">Saldo: <span className="money">{user.balance}</span></p>
             <div>
-                <h3 className="title">Extrato da conta <span><img alt="down" src={down}/></span></h3>
-                <div className="operation_list">
-                    <div className="operation">
-                        <p className="type">Transferência de <span className="money">30.35</span> Para Maria</p>
-                        <p className="date">23:30 03-05-2020</p>
-                    </div>
-                </div>
+                <button className="title" onClick={() => {
+                    if (loadHistory)
+                        setLoadHistory(false);
+                    else setLoadHistory(true);
+                    console.log(loadHistory);
+
+                    var h = user;
+                    if (loadHistory && user)
+                        axios.get(`${api_url}/Operation/${user.password}/${user.id}`).then((r) => {
+                            h = r.data;
+                            console.log(h);
+
+                            setHistory(h);
+                        });
+
+                    else {
+                        console.log('NULLing');
+                        h = null
+                        setHistory(h);
+                    }
+
+                    console.log(user);
+                }}><h3 className="title">Extrato da conta <span><img alt="down" src={down}/></span></h3></button>
+
+                { loadHistory && history ? history.map((o) => {
+                    console.log("JOAO!!!!!");
+                    console.log(o);
+                    return (<div className="operation-list">
+                        {o.type !== "GetClient" ?(<div className="operation">
+                            <p className="type">
+                                {getType(o.type)}
+                                { o.value ? `de <span className="money">${o.value}</span>` : "" }
+                                { o.receiver ? `Para ${o.receiver}` : "" }
+                                { o.sender && o.sender !== user.id.ToString() ? `De ${o.sender}` : "" }
+                            </p>
+                            <p className="date">{ o.date }</p>
+                        </div>):''}
+                    </div>);
+
+                }) : <p></p> }
+
             </div>
         </div>
     );
