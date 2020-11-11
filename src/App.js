f935ceeb8751d8870b0dd331aba0f7172d4246ca
Plankiton <pl4nk1ton@gmail.com>
2020-11-11 03:58:48 -0300

    Corrige bugs e melhora aparencia do codigo

--- src/App.js
+++ src/App.js
@@ -24,10 +24,38 @@ function getType(t) {
     }
 }
 
-function sleep(s) {
-  return new Promise(
-    resolve => setTimeout(resolve, s*10)
-  );
+
+function getUser(pass, id) {
+    return axios.get(`${api_url}/Client/${pass}/${id}`, conf);
+}
+
+function getUserByName(pass, name) {
+    return axios.get(`${api_url}/Client/byname/${pass}/${name}`, conf);
+}
+
+async function getNameFromId(id) {
+    var r = await axios.get(`${api_url}/Client/${id}`, conf);
+    return r.data.name;
+}
+
+function getHistory(pass, id) {
+    return axios.get(`${api_url}/Operation/${pass}/${id}`, conf);
+}
+
+function postUser(data){
+    return axios.post(`${api_url}/Client`, data, conf);
+}
+
+function takeIn(data){
+    return axios.post(`${api_url}/TakeIn`, data, conf);
+}
+
+function takeOut(data){
+    return axios.post(`${api_url}/TakeOut`, data, conf);
+}
+
+function trade(data){
+    return axios.post(`${api_url}/Trade`, data, conf);
 }
 
 function App() {
@@ -35,31 +63,13 @@ function App() {
     const [history, setHistory] = useState([]);
 
     useEffect(() => {
-        async function updateData(){
-            if (user){
+        if (user)
+            getHistory(user.password, user.id).then((r) => {
+                if (history !== r.data){
+                    setHistory(r.data);
+                }
+            });
 
-                axios.get(`${api_url}/Client/${user.password}/${user.id}`, conf).then((r) => {
-                    console.log(r);
-                    if (r.data.balance !== user.balance){
-                        setUser({
-                            id: r.data.id,
-                            name: r.data.name,
-                            password: user.password,
-                            balance: r.data.balance,
-                        });
-
-                        axios.get(`${api_url}/Operation/${user.password}/${user.id}`, conf).then((r) => {
-                            if (r.data !== history){
-                                setHistory(r.data);
-                                console.log(r.data);
-                            }
-                        });
-                    }});
-
-            }
-        }
-
-        updateData();
     });
 
     return user == null ? (
@@ -69,21 +79,21 @@ function App() {
 
                 var name = e.target[0].value;
                 var pass = e.target[1].value;
-                axios.get(`${api_url}/Client/byname/${pass}/${name}`, conf)
+                getUserByName(pass, name)
                     .then((r) => {
-                        console.log(r);
+
                         setUser({
                             id: r.data.id,
                             name: r.data.name,
                             password: pass,
                             balance: r.data.balance,
                         });
+
                     }).catch((e) => {
-                        axios.post(`${api_url}/Client`, {
+                        postUser({
                             name: name,
                             password: pass
-                        }, conf).then((r) => {
-                            console.log(r);
+                        }).then((r) => {
 
                             setUser({
                                 id: r.data.id,
@@ -91,13 +101,11 @@ function App() {
                                 password: pass,
                                 balance: r.data.balance,
                             });
-                        }).catch((e) => {
-                            console.log(e);
-                        }).finally(() => {
-                            console.log(pass, name);
+
                         });
 
-                    });
+                    })
+
 
             }}>
                 <h1>Login</h1>
@@ -113,19 +121,39 @@ function App() {
         </div>
     ) : (
         <div id="conta">
-            <h1 className="user">{user.name}</h1>
-            <p className="balance">Saldo: <span className="money">{user.balance.toFixed(2)} reais</span></p>
+            <h1 className="user">{user.name} <span className="id">id:{user.id}</span></h1>
+            <p className="balance">Saldo: <span className="money">{user.balance.toFixed(2)} reais</span> 
+            <button className="reload" onClick={() => {
+                getUser(user.password, user.id).then((r) => {
+
+                    if (r.data.balance !== user.balance){
+                        setUser({
+                            id: r.data.id,
+                            name: r.data.name,
+                            password: user.password,
+                            balance: r.data.balance,
+                        });
+
+                        getHistory(user.password, user.id).then((r) => {
+                            setHistory(r.data);
+                        });
+
+                    }
+
+                });
+            }}><img alt="reload" src="https://img.icons8.com/ios-filled/50/000000/update-left-rotation.png"/></button></p>
 
             <form className="function" onSubmit={(e) => {
                 e.preventDefault();
-                console.log(parseFloat(e.target[0].value));
-                axios.post(`${api_url}/TakeIn`, {
+
+                takeIn({
                     clientid: user.id,
                     value: parseFloat(e.target[0].value),
                     password: user.password,
-                }, conf).then((r) => {
-                    axios.get(`${api_url}/Client/${user.password}/${user.id}`, conf).then((r) => {
-                        console.log(r);
+                }).then((r) => {
+
+                    getUser(user.password, user.id).then((r) => {
+
                         if (r.data.balance !== user.balance){
                             setUser({
                                 id: r.data.id,
@@ -133,25 +161,32 @@ function App() {
                                 password: user.password,
                                 balance: r.data.balance,
                             });
-                        }});
-                }).catch((e) => {
-                    console.log(e);
+
+                            getHistory(user.password, user.id).then((r) => {
+                                setHistory(r.data);
+                            });
+
+                        }
+
+                    });
+
                 });
-                }}>
+            }}>
                 <label htmlFor="takein">Depositar dinheiro:</label>
                 <input type="text" name="takein" id="takein" placeholder="Valor do deposito"/>
                 <input type="submit" value="Depositar"/>
             </form>
             <form className="function" onSubmit={(e) => {
                 e.preventDefault();
-                console.log(parseFloat(e.target[0].value));
-                axios.post(`${api_url}/TakeOut`, {
+
+                takeOut({
                     clientid: user.id,
                     password: user.password,
                     value: parseFloat(e.target[0].value)
-                }, conf).then((r) => {
-                    axios.get(`${api_url}/Client/${user.password}/${user.id}`, conf).then((r) => {
-                        console.log(r);
+                }).then((r) => {
+
+                    getUser(user.password, user.id).then((r) => {
+
                         if (r.data.balance !== user.balance){
                             setUser({
                                 id: r.data.id,
@@ -159,26 +194,35 @@ function App() {
                                 password: user.password,
                                 balance: r.data.balance,
                             });
-                        }});
-                }).catch((e) => {
-                    console.log(e);
+
+                            getHistory(user.password, user.id).then((r) => {
+                                setHistory(r.data);
+                            });
+
+                        }
+
+                    });
+
                 });
-                }}>
+
+            }}>
                 <label htmlFor="takeout">Sacar dinheiro:</label>
                 <input type="text" name="takeout" id="takeout" placeholder="Valor do saque"/>
                 <input type="submit" value="Sacar"/>
             </form>
+
             <form className="function" onSubmit={(e) => {
                 e.preventDefault();
-                console.log(parseFloat(e.target[0].value));
-                axios.post(`${api_url}/Trade`, {
+
+                trade({
                     senderid: user.id,
                     password: user.password,
                     receiverid: parseInt(e.target[1].value),
                     value: parseFloat(e.target[0].value)
-                }, conf).then((r) => {
-                    axios.get(`${api_url}/Client/${user.password}/${user.id}`, conf).then((r) => {
-                        console.log(r);
+                }).then((r) => {
+
+                    getUser(user.password, user.id).then((r) => {
+
                         if (r.data.balance !== user.balance){
                             setUser({
                                 id: r.data.id,
@@ -186,15 +230,22 @@ function App() {
                                 password: user.password,
                                 balance: r.data.balance,
                             });
-                        }});
-                }).catch((e) => {
-                    console.log(e);
+
+                            getHistory(user.password, user.id).then((r) => {
+                                setHistory(r.data);
+                            });
+
+                        }
+
+                    });
+
                 });
-                }}>
+
+            }}>
                 <label htmlFor="trade">Transferir dinheiro:</label>
                 <input type="text" name="trade" id="trade" placeholder="Valor da transferencia"/>
                 <label htmlFor="receiver">Destinário:</label>
-                <input type="text" name="receiver" id="receiver" placeholder="Username do receiver"/>
+                <input type="text" name="receiver" id="receiver" placeholder="ID do receiver"/>
                 <input type="submit" value="Transferencia"/>
             </form>
 
@@ -206,12 +257,17 @@ function App() {
 
                         var d = new Date(o.date);
                         o.date = d.toString().substring(0, 21);
+                        var receiver = o.receiver;
+                        var sender = o.sender;
+
                         return o.type !== "GetClient" ? (<div className="operation">
                             <p className="type">
                                 {getType(o.type)}
                                 { o.value ? (<span> de <span className="money">{parseFloat(o.value).toFixed(2)} reais</span></span>): "" }
-                                { o.receiver ? ` para ${o.receiver}` : "" }
-                                { o.sender && o.sender !== user.id.ToString() ? `de ${o.sender}` : "" }
+                                { o.receiver && o.receiver !== user.id.toString()  ?
+                                        (<span> para <span key="recv" className="id">Id:{receiver}</span></span>) : "" }
+                                { o.sender && o.sender !== user.id.toString() ?
+                                        (<span> de <span className="id">Id:{sender}</span></span>) : "" }
                             </p>
                             <p className="date">{ o.date }</p>
                         </div>) : '';
